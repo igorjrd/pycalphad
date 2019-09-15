7acd9f7c0e1dddce3433c0d7b6f5552a9d11d202
Igor J Marques <igorjrd2@gmail.com>
2019-09-15 13:19:26 -0300

    FIX: pure vacancy phase detection

--- pycalphad/model.py
+++ pycalphad/model.py
@@ -152,6 +152,9 @@ class Model(object):
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> FIX: pure vacancy phase detection
 =======
 >>>>>>> FIX: pure vacancy phase detection
             is_pure_VA.add(sum(set(map(lambda s : getattr(s, 'number_of_atoms'),sublattice_comps))))
@@ -198,6 +201,16 @@ class Model(object):
 =======
 =======
 >>>>>>> FIX: pure vacancy phase detection
+>>>>>>> FIX: pure vacancy phase detection
+=======
+            is_pure_VA.add(sum(set(map(lambda s : getattr(s, 'number_of_atoms'),sublattice_comps))))
+            self.constituents.append(sublattice_comps)
+        if sum(is_pure_VA) == 0:
+            #The only possible component in a sublattice is vacancy
+            #We cannot build a model of this phase
+            raise DofError(
+                '{0}: Sublattices of {1} contains only VA (VACUUM) constituents' \
+                .format(self.phase_name, phase.constituents))
 >>>>>>> FIX: pure vacancy phase detection
         self.components = sorted(self.components)
         desired_active_pure_elements = [list(x.constituents.keys()) for x in self.components]
